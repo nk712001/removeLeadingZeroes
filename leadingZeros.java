69c717c36d29bb79822d9faec5ee36a81ef52efb
Spr_zero <81459719+nk712001@users.noreply.github.com>
2021-11-15 21:38:31 +0530

    Add files via upload

---
+++ leadingZeros.java
@@ -0,0 +1,27 @@
+package assingmentEdyoda3;
+import java.util.Arrays;
+import java.util.List;
+public class leadingZeros {
+    public static String removeZero(String str)
+    {
+
+        int i = 0;
+        while (i < str.length() && str.charAt(i) == '0')
+            i++;
+
+        StringBuffer sb = new StringBuffer(str);
+
+
+        sb.replace(0, i, "");
+
+        return sb.toString();
+    }
+
+
+    public static void main (String[] args)
+    {
+        String str = "00000123569";
+        str = removeZero(str);
+        System.out.println(str);
+    }
+}
